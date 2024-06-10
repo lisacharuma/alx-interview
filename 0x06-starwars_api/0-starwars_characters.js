b2b1d83e6a45b8ca56947adf893725c3032c402d
lisacharuma <charumalisah@gmail.com>
2024-06-10 14:10:35 +0000

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,35 @@
 #!/usr/bin/node
-const request = require('request');
-const movie = process.argv[2];
-request(`https://swapi-api.hbtn.io/api/films/${movie}`, function (error, response, body) {
-  if (error) {
-    return console.log(error);
-  }
-  const characters = JSON.parse(body).characters;
-  characters.forEach(element => {
-    request(element, function (error, response, body) {
-      if (error) {
-        return console.log(error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
-});
+
+const axios = require('axios');
+const process = require('process');
+
+if (process.argv.length !== 3) {
+    console.log("Usage: node star_wars_characters.js <Movie ID>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+
+const getMovieCharacters = async (movieId) => {
+    try {
+        // Base URL for the Star Wars API
+        const baseURL = 'https://swapi.dev/api';
+        
+        // Fetch the movie details from the API
+        const movieURL = `${baseURL}/films/${movieId}/`;
+        const response = await axios.get(movieURL);
+        
+        // Get the list of character URLs
+        const characters = response.data.characters;
+        
+        // Fetch and print each character's name
+        for (const characterURL of characters) {
+            const charResponse = await axios.get(characterURL);
+            console.log(charResponse.data.name);
+        }
+    } catch (error) {
+        console.error(`Error: Unable to fetch movie with ID ${movieId}`);
+    }
+};
+
+getMovieCharacters(movieId);
